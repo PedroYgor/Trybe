14ce1b5a100d44283ac1a67e30958ebe5d8ea129
Pedro Ygor <pedroygor2010@hotmail.com>
2022-05-12 02:58:04 -0300

    add exercicios

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01/1.1/script.js
@@ -0,0 +1,7 @@
+const myName = 'Pedro Ygor';
+const birthCity = 'Banabuiú';
+var birthYear = 2030;
+
+// console.log(`Meu nome ${myName}, cidade natal ${birthCity} e ano de nascimento ${birthYear}`);
+
+console.log(birthCity);
---
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01/1.2/script.js
@@ -0,0 +1,19 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+const base = 5;
+const height = 8;
+const area = base * height;
+console.log(area);
+const retangulo = {
+  lado1: 10,
+  lado2: 12,
+  lado3: 9,
+  lado4: 12
+}
+const perimeter = retangulo.lado1 + retangulo.lado2 + retangulo.lado3 + retangulo.lado4;
+console.log(perimeter);
