0c750cbc90b4f6447b9864e360d9bb580536eae5
Pedro Ygor <pedroygor2010@hotmail.com>
2022-06-20 01:17:44 -0300

    exercício dia 3 finalizado

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03/sum.test.js
@@ -2,11 +2,11 @@ const sum = require('./sum.js');
 
 describe('function sum()', () => {
   it('verify if 4 + 5 to equals 9', () => {
-    expect(sum(4, 5)).toEqual(9)
+    expect(sum(4, 5)).toBe(9)
   })
 
   it('verify if 0 + 0 to equals 0', () => {
-    expect(sum(0, 0)).toEqual(0)
+    expect(sum(0, 0)).toBe(0)
   })
 
   it('verify if return throw new error', () => {
