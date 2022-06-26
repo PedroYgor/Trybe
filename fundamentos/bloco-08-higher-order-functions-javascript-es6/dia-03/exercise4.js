1afb72e03f4905987bb3f5b454a8e40125bdcba6
Pedro Ygor <pedroygor2010@hotmail.com>
2022-06-26 01:10:10 -0300

    Bloco 8 - dia 3, questão 4

---
+++ fundamentos/bloco-08-higher-order-functions-javascript-es6/dia-03/exercise4.js
@@ -0,0 +1,39 @@
+const books = require("./books");
+
+  const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+// Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+// Dica: use as funções filter e sort
+function oldBooksOrdered() {
+  return books.filter(item => {
+    let idade = 2022 - item.releaseYear;
+    return idade > 60;
+  }).sort((bookA, bookB) => {
+    if (bookA.releaseYear < bookB.releaseYear) return -1;
+    if (bookA.releaseYear > bookB.releaseYear) return 1;
+    return 0;
+  })
+}
+
+console.log(oldBooksOrdered());
