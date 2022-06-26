8a4f5a1e6b0ef625b3437337c5be087cdea11e2f
Pedro Ygor <pedroygor2010@hotmail.com>
2022-06-26 15:33:37 -0300

    Instalando o eslint no módulo de fundamentos

--- fundamentos/bloco-08-higher-order-functions-javascript-es6/dia-03/exercise6.js
+++ fundamentos/bloco-08-higher-order-functions-javascript-es6/dia-03/exercise6.js
@@ -1,19 +1,20 @@
-const books = require('./books.js')
-const expectedResult = [
-  'O Senhor dos Anéis',
-  'Fundação',
-  'O Chamado de Cthulhu'
-]
+const books = require('./books');
+
+// const expectedResult = [
+//   'O Senhor dos Anéis',
+//   'Fundação',
+//   'O Chamado de Cthulhu',
+// ];
 // 6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
 function oldBooks() {
   return books
-    .filter(item => {
-      let currentYear = new Date()
-      let year = currentYear.getFullYear()
-      let bookAge = year - item.releaseYear
-      return bookAge > 60
+    .filter((item) => {
+      const currentYear = new Date();
+      const year = currentYear.getFullYear();
+      const bookAge = year - item.releaseYear;
+      return bookAge > 60;
     })
-    .map(book => book.name)
+    .map((book) => book.name);
 }
 
-console.log(oldBooks())
+console.log(oldBooks());
