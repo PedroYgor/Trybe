fe7863e2cfa369ec7b6aa3c80f680e0e41f8ea10
Pedro Ygor <pedroygor2010@hotmail.com>
2022-06-26 02:22:23 -0300

    Bloco 8 - dia 3, questão 7

---
+++ fundamentos/bloco-08-higher-order-functions-javascript-es6/dia-03/exercise7.js
@@ -0,0 +1,12 @@
+const books = require('./books.js');
+// Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+// Dica: cada inicial termina com um ponto.
+
+const expectedResult = 'O Senhor dos Anéis';
+const regex = /[a-z](\.)\s[a-z](\.)\s[a-z](\.)/i
+function authorWith3DotsOnName() {
+  return books
+  .filter(book => regex.test(book.author.name))
+  .map(book => book.name)
+}
+console.log(authorWith3DotsOnName());
