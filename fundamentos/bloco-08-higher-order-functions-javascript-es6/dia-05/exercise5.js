2c49d5797e7d03afd1215350033a91cb99c11e30
Pedro Ygor <pedroygor2010@hotmail.com>
2022-06-29 00:06:03 -0300

    Bloco 8 - dia 5, exercício 5

---
+++ fundamentos/bloco-08-higher-order-functions-javascript-es6/dia-05/exercise5.js
@@ -0,0 +1,7 @@
+// 5 - Escreva a função swap, que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
+const myList = [1, 2, 3];
+ [a, b, c] = myList.reverse();
+ console.log(a);
+ console.log(b);
+ console.log(c);
+
