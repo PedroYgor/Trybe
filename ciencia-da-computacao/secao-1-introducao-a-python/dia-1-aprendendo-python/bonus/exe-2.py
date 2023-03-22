733744f1d11cc57175d58af130d322e095f5f11a
pedroygor <pedroygor2010@hotmail.com>
2023-03-21 22:20:28 -0300

    feat: desenha asteriscos em sequência

---
+++ ciencia-da-computacao/secao-1-introducao-a-python/dia-1-aprendendo-python/bonus/exe-2.py
@@ -0,0 +1,15 @@
+# Exercício 2: Faça um programa que, dado um valor n qualquer, tal que n > 1,
+# imprima na tela um triângulo retângulo com n asteriscos de base.
+# Por exemplo, para n = 5 o triângulo terá 5 asteriscos na base:
+
+
+def desenha_asteriscos_em_sequencia(n: int):
+    asteriscos = ""
+    for i in range(n):
+        quantidade_asteriscos = i + 1
+        asteriscos += quantidade_asteriscos * "*" + "\n"
+
+    return asteriscos
+
+
+print(desenha_asteriscos_em_sequencia(5))
