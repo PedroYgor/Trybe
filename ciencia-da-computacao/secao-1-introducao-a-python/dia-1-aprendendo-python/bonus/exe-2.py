d4e826bebca5b10b9c87bec83cddfa952619f5cb
pedroygor <pedroygor2010@hotmail.com>
2023-03-21 23:16:40 -0300

    refactor:  tipando funções

--- ciencia-da-computacao/secao-1-introducao-a-python/dia-1-aprendendo-python/bonus/exe-2.py
+++ ciencia-da-computacao/secao-1-introducao-a-python/dia-1-aprendendo-python/bonus/exe-2.py
@@ -2,8 +2,11 @@
 # imprima na tela um triângulo retângulo com n asteriscos de base.
 # Por exemplo, para n = 5 o triângulo terá 5 asteriscos na base:
 
+# Estou gostando de tipar as funções, acho que ajuda
+# a entender melhor o que está acontecendo
 
-def desenha_asteriscos_em_sequencia(n: int):
+
+def desenha_asteriscos_em_sequencia(n: int) -> str:
     asteriscos = ""
     for i in range(n):
         quantidade_asteriscos = i + 1
