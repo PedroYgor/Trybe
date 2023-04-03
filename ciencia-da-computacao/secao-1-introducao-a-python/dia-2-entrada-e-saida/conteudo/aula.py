05107c08852404a79c6af9ac712b84d7dbe61c3a
pedroygor <pedroygor2010@hotmail.com>
2023-04-02 21:41:09 -0300

    conteúdo

---
+++ ciencia-da-computacao/secao-1-introducao-a-python/dia-2-entrada-e-saida/conteudo/aula.py
@@ -0,0 +1,13 @@
+mensagem = input("Digite uma mensagem: ")
+
+
+print("Você digitou: " + mensagem)
+
+
+# Exercício 1 - Faça um programa que solicite o nome de uma pessoa usuária e imprima-o na vertical.
+# Exemplo:
+
+nome = input("Digite seu nome: ")
+
+for letra in nome:
+    print(letra)
